f61e05df86117203a964398fee420b92934f4688
stuart_robert27 <stuart_robert27@hotmail.com>
2023-01-15 13:07:44 +0000

    simple path params working

--- README.md
+++ README.md
@@ -4,13 +4,23 @@ Seems promising, especially as built of the Tokio/Tower stack.
 
 ## Things To Tick Off
 
-In no particular order
+In no particular order:
 
-- path params
-- send JSON (with param based input) -- correct mime type
-- peceive a struct and de-serialize it (POST body)
-- serve static files
-- read / add headers
-- cookies 
-- TLS / HTTPS
-- User based security 
+- [x] path params
+- [ ] send JSON (with param based input) -- correct mime type
+- [ ] peceive a struct and de-serialize it (POST body)
+- [ ] use some form of app state in handler .. like a cache etc
+- [ ] errors on de-serialize
+- [ ] Option?? in Params and structs
+- [ ] serve static files
+- [ ] read / add headers
+- [ ] cookies 
+- [ ] TLS / HTTPS
+- [ ] user based security 
+- [ ] file upload
+
+# Learning
+
+(Tokio, Tower and async)
+
+Rust async functions de-sugar into futures.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+
 use axum::{
     routing::get,
     Router,
+    extract::Path
 };
 
 
@@ -8,11 +10,29 @@ use axum::{
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/hello/:name", get(hello_name))
+        .route("/hello/:name/:id", get(hello_name_and_number))
+        ;
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+/// Simple param1: single parameter mapped from URL
+async fn hello_name(Path(id): Path<String>) -> String {
+    println!("I got {id}");
+    format!("Hello {id} I hope you are well")
+}
+
+
+/// Simple param take ii - take 2 parameters mapped from same URL 
+/// NB dont double-parenthesis on param, the inner ones are for the tuple
+async fn hello_name_and_number(Path((id, n)): Path<(String, u32)>) -> String {
+    println!("I got {id}/{n}");
+    format!("Hello {id}, the number you supplied was {n}")
 }
